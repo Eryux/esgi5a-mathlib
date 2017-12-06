a4ad62050d0fe110271eb25aaffff14eabe32349
Foucauld <foucauld.bouvattier@gmail.com>
2017-12-06 22:29:21 +0100

    vector from points + comments

--- MathLib/MathLib/Utils.cpp
+++ MathLib/MathLib/Utils.cpp
@@ -1,6 +1,8 @@
 #include "Utils.h"
 #include <math.h>
 
+#define PI 3.14159265359
+
 float Utils::jarvis_angle(glm::vec2 director_vec, glm::vec2 pk_pj)
 {
 	return acos(scalar_product(director_vec, pk_pj)/(norm(director_vec)*norm(pk_pj)));
@@ -12,4 +14,9 @@ float Utils::scalar_product(glm::vec2 vector1, glm::vec2 vector2) {
 
 float Utils::norm(glm::vec2 vector) {
 	return sqrt(scalar_product(vector, vector));
+}
+
+glm::vec2 Utils::get_vector_from_points(glm::vec2 point1, glm::vec2 point2)
+{
+	return glm::vec2(point2.x - point1.x, point2.y - point1.y);
 }
